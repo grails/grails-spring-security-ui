906517d5dc31d4e8d16afa3646cc0d08ea6042fd
Graeme Rocher <graeme.rocher@gmail.com>
2014-05-20 09:09:01 +0200

    upgrade dependencies to more recent versions

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,7 @@
 grails.project.work.dir = 'target'
 grails.project.docs.output.dir = 'docs/manual' // for backwards-compatibility, the docs are checked into gh-pages branch
 
+grails.project.dependency.resolver="maven"
 grails.project.dependency.resolution = {
 
 	inherits 'global'
@@ -11,11 +12,12 @@ grails.project.dependency.resolution = {
 		mavenLocal()
 		mavenCentral()
 		mavenRepo 'http://download.java.net/maven/2/'
+		mavenRepo 'http://repo.spring.io/milestone'
 	}
 
 	plugins {
-		compile ':spring-security-core:2.0-RC2'
-		compile ':mail:1.0.1'
+		compile ':spring-security-core:2.0-RC3'
+		compile ':mail:1.0.5'
 		compile ':jquery:1.10.2'
 		compile ':jquery-ui:1.8.24'
 		compile ':famfamfam:1.0.1'
@@ -24,11 +26,11 @@ grails.project.dependency.resolution = {
 			export = false
 		}
 
-		compile ":hibernate:$grailsVersion", {
+		compile ":hibernate:3.6.10.14", {
 			export = false
 		}
 
-		build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+		build ':release:3.0.1', ':rest-client-builder:2.0.1', {
 			export = false
 		}
 	}
