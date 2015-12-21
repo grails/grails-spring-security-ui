f16c2415b66c9bb589f002d4597881abe47fb221
Burt Beckwith <burt@burtbeckwith.com>
2015-12-20 23:50:16 -0500

    test fix

--- src/test/groovy/grails/plugin/springsecurity/ui/RegisterControllerSpec.groovy
+++ src/test/groovy/grails/plugin/springsecurity/ui/RegisterControllerSpec.groovy
@@ -1,19 +1,13 @@
 package grails.plugin.springsecurity.ui
 
-import grails.core.DefaultGrailsApplication
-import grails.plugin.springsecurity.ReflectionUtils
 import grails.plugin.springsecurity.SpringSecurityUtils
 import spock.lang.Specification
 
 class RegisterControllerSpec extends Specification {
 
-	void setupSpec() {
-		ReflectionUtils.application = new DefaultGrailsApplication()
-		updateFromConfig()
-	}
-
 	void setup() {
 		SpringSecurityUtils.setSecurityConfig [:] as ConfigObject
+		updateFromConfig()
 	}
 
 	void cleanup() {
