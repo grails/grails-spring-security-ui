44efce3b13b7b3e5eebd2b51d333b4f2a8f1920b
Burt Beckwith <burt@burtbeckwith.com>
2016-04-14 20:02:50 -0400

    explicitly using 5.0.x versions of hibernate4 plugin and grails-datastore-* dependencies so the compiled RegistrationCode domain class works in 3.0 and 3.1; fixes #41

--- functional-test-app/run_functional_tests.sh
+++ functional-test-app/run_functional_tests.sh
@@ -5,12 +5,12 @@ cd "$DIR"
 
 set -e
 
-# doesn't work with 3.1 yet because the compiled domain class references org.grails.datastore.gorm.GormEntity in org.grails:grails-datastore-gorm-support and this results in a NoClassDefFoundError at startup
-# GRAILS_VERSIONS="3.0.11 3.1.0.RC1"
-GRAILS_VERSIONS="3.0.11"
+GRAILS_VERSIONS="3.0.16 3.1.5"
 TEST_GROUPS="simple extended"
 
 rm -rf build
+rm -f build*.gradle
+rm -f gradle*.properties
 
 function generateBuildGradle {
 	testGroup=$1
@@ -29,17 +29,26 @@ function generateBuildGradle {
 
 	echo -e "\ndependencies {" >> build.gradle
 	echo -e "\tcompile 'dumbster:dumbster:1.6', { transitive = false }" >> build.gradle
-	echo -e "\tcompile 'org.grails.plugins:mail:2.0.0.RC4'" >> build.gradle
+	echo -e "\tcompile 'org.grails.plugins:mail:2.0.0.RC6'" >> build.gradle
+
 	if [[ $testGroup = 'extended' ]]; then
 		echo -e "\tcompile 'org.grails.plugins:spring-security-acl:3.0.1'" >> build.gradle
 	fi
-	if [[ $grailsVersion =~ 3\.0\..+ ]]; then
-		echo -e "\tcompile 'org.grails.plugins:hibernate'" >> build.gradle
-	else
-		echo -e "\tcompile 'org.grails.plugins:hibernate4'" >> build.gradle
+
+	if [[ $grailsVersion =~ 3\.1\..+ ]]; then
 		echo -e "\tcompile 'org.grails:grails-core'" >> build.gradle
 		echo -e "\tprofile 'org.grails.profiles:web:$grailsVersion'" >> build.gradle
 	fi
+
+	echo -e "\tcompile 'com.h2database:h2:1.4.191'" >> build.gradle
+
+	GORM_VERSION="5.0.4"
+	GORM_NAMES="core gorm-support gorm simple gorm-hibernate4 gorm-hibernate-core"
+	for gormName in $GORM_NAMES; do
+		echo -e "\tcompile 'org.grails:grails-datastore-$gormName:$GORM_VERSION.RELEASE'" >> build.gradle
+	done
+	echo -e "\tcompile 'org.grails.plugins:hibernate4:$GORM_VERSION'" >> build.gradle
+
 	echo "}" >> build.gradle
 
 	echo "$(<gradle/integrationTest.inc)" >> build.gradle
