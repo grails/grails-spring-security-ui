b41c154dfbbba097f5cce79f39ca8fb02aac61ba
sdelamo <sergio.delamo@softamo.com>
2017-12-05 17:03:25 +0100

    run tests in chrome headless travis

--- examples/extended/src/integration-test/groovy/GebConfig.groovy
+++ examples/extended/src/integration-test/groovy/GebConfig.groovy
@@ -1,27 +1,20 @@
 // See: http://www.gebish.org/manual/current/configuration.html
-import org.openqa.selenium.htmlunit.HtmlUnitDriver
+import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.chrome.ChromeDriver
-import org.openqa.selenium.firefox.FirefoxDriver
-import org.openqa.selenium.phantomjs.PhantomJSDriver
-import org.openqa.selenium.remote.DesiredCapabilities
-
 quitCachedDriverOnShutdown = true
 
 environments {
 
-	htmlUnit {
-		driver = { new HtmlUnitDriver() }
-	}
-
-	phantomjs {
-		driver = { new PhantomJSDriver(new DesiredCapabilities()) }
-	}
 
 	chrome {
 		driver = { new ChromeDriver() }
 	}
 
-	firefox {
-		driver = { new FirefoxDriver() }
+	chromeHeadless {
+		driver = {
+			ChromeOptions o = new ChromeOptions()
+			o.addArguments('headless')
+			new ChromeDriver(o)
+		}
 	}
 }
