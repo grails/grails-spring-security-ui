cf21e544d43f64c8106a43e33d06665a03a297f5
Dean Del Ponte <dean.delponte@gmail.com>
2019-04-11 10:43:20 -0500

    Issue 108: Verifying a new user account does not unlock that account - added a test to verify the verify registration functionality is working as expected

---
+++ examples/extended/src/integration-test/groovy/spec/VerifyRegistrationSpec.groovy
@@ -0,0 +1,56 @@
+package spec
+
+import grails.plugin.springsecurity.ui.RegistrationCode
+import grails.plugins.rest.client.RestBuilder
+import grails.plugins.rest.client.RestResponse
+import grails.testing.mixin.integration.Integration
+import spock.lang.Shared
+import spock.lang.Specification
+import test.User
+
+
+@Integration
+class VerifyRegistrationSpec extends Specification {
+
+    @Shared RestBuilder rest = new RestBuilder()
+
+    void "verify a call to the register/verifyRegistration endpoint properly updates the verified"() {
+        given: "a username"
+        String username = "username"
+
+        when: "a user account is created and given a registration code"
+        User user
+        RegistrationCode registrationCode
+
+        User.withNewTransaction {
+            user = new User(username: username, password: "password", accountLocked: true).save()
+            registrationCode = new RegistrationCode(username: username).save()
+        }
+
+
+        then: 'registration code token is populated'
+        registrationCode.token
+        user.accountLocked == true
+        user.accountExpired == false
+        user.enabled == true
+        user.username == username
+
+        when: "that user engages the verify registration action with their registration code's token"
+        RestResponse resp = rest.get("http://localhost:${serverPort}/register/verifyRegistration?t=${registrationCode.token}")
+
+        then: "that user account should be unlocked, not expired, and enabled"
+        resp.status == 200
+
+        when:
+        User updatedUser = User.withNewTransaction(readOnly: true) {
+            User.findByUsername(username)
+        }
+
+        then:
+        updatedUser.username == username
+        updatedUser.accountExpired == false
+        updatedUser.enabled == true
+        updatedUser.accountLocked == false
+
+    }
+}
